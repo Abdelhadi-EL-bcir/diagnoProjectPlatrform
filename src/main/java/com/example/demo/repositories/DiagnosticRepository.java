c062012624f4d810529a821b0b7cee59c191b7a7
abdelhadi-22 <abdoelbcir33@gmail.com>
2023-10-04 16:07:54 +0100

    creating service and controller for user

--- src/main/java/com/example/demo/repositories/DiagnosticRepository.java
+++ src/main/java/com/example/demo/repositories/DiagnosticRepository.java
@@ -1,12 +1,9 @@
 package com.example.demo.repositories;
 
-import com.example.demo.beans.User;
+import com.example.demo.beans.Diagnostic;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User , Long> {
-
-    public User findUserById(Long id);
-
+public interface DiagnosticRepository extends JpaRepository<Diagnostic , Long> {
 }
